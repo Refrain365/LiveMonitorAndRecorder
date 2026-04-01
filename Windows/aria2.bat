0fdbdd3a9bf5f222d04d6f0d69f5515248fd879b
真理的中点 <3047278723@qq.com>
2026-04-01 18:12:33 +0800

    外在依赖#2：aria2启动脚本
    优化初始化参数配置，同时进行任务数增至100

--- Windows/aria2.bat
+++ Windows/aria2.bat
@@ -8,9 +8,8 @@ REM 移除末尾的反斜杠（如果有）
 if "!SCRIPT_DIR:~-1!"=="\" set "SCRIPT_DIR=!SCRIPT_DIR:~0,-1!"
 
 REM 启动aria2，设置工作目录为脚本所在目录
-start /min aria2c --enable-rpc --rpc-listen-all --disable-ipv6=true --dir="!SCRIPT_DIR!"
+start /min aria2c --enable-rpc --rpc-listen-all --disable-ipv6=true --max-concurrent-downloads=100 --max-connection-per-server=16 --timeout=120 --dir="!SCRIPT_DIR!"
 
 echo Aria2已启动，工作目录设置为: !SCRIPT_DIR!
 echo 按任意键退出...
-
 pause >nul
